c22190408834969d4748c193d9525dfc0d44914d
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-24 10:45:44 -0700

    feat(locals): added fem-eci-github workspace

--- locals.tf
+++ locals.tf
@@ -19,5 +19,12 @@ locals {
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
+
+    "fem-eci-github" = {
+      description         = "Example automation workspace for GitHub resources."
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+    }
   }
 }
