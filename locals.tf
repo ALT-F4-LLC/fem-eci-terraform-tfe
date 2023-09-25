27f4746a88d468a5b4230d6bf26de1681b483b3d
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-24 18:52:24 -0700

    feat: added service infra automation

--- locals.tf
+++ locals.tf
@@ -82,5 +82,25 @@ locals {
         },
       ]
     }
+
+    "fem-eci-product-service-prod" = {
+      description         = "Automation for product service resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-product-service"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "cluster_name"
+          value    = "fem-eci-<your-nam>-prod"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+      ]
+    }
   }
 }
