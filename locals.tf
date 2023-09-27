7e4c4cf31ee5b582ec172a64564dca161a05d690
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-26 21:00:45 -0500

    fix(*): remove team

--- locals.tf
+++ locals.tf
@@ -1,14 +1,7 @@
 locals {
-  team = {
-    "fem-eci-team" = {
-      usernames = var.organization_usernames
-    }
-  }
-
   project = {
     "fem-eci-project" = {
       description = "Example description of project"
-      team_ids    = [module.team["fem-eci-team"].id]
     }
   }
 
