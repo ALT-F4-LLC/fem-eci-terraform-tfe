8ac0fd7302797096bbbc688213108832d5a3e210
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-26 20:54:29 -0500

    fix(*): remove teams

--- variables.tf
+++ variables.tf
@@ -12,8 +12,3 @@ variable "organization_name" {
   default = "<your-organization-name>"
   type    = string
 }
-
-variable "organization_usernames" {
-  default = ["<your-organization-username>"]
-  type    = list(string)
-}
